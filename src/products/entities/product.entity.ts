9b5c78b92f59d412bfc7c1adf281f2c888b6423d
Leandro Garzon <hekoej@gmail.com>
2024-10-05 15:54:39 -0500

    BeforeInsert y BeforeUpdate

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Product {
@@ -40,5 +40,21 @@ export class Product {
   @Column('text')
   gender: string;
 
+
+  @BeforeInsert()
+  checkSlugInsert() {
+
+    if( !this.slug ) {
+      this.slug = this.title;
+    }
+
+    this.slug = this.slug
+      .toLowerCase()
+      .replaceAll(' ', '_')
+      .replaceAll("'", "")
+
+  }
+
+  
 
 }
