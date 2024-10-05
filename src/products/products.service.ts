961860ce7ae630e9830537934859f2db5b85df87
Leandro Garzon <hekoej@gmail.com>
2024-10-05 17:03:47 -0500

    Paginar en TypeORM

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -4,6 +4,7 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
 @Injectable()
 export class ProductsService {
@@ -35,8 +36,12 @@ export class ProductsService {
 
   }
 
-  findAll() {
-    return this.productRepository.find({});
+  findAll( paginationDto: PaginationDto ) {
+    const { limit = 10, offset = 0 } = paginationDto;
+    return this.productRepository.find({
+      take: limit,
+      skip: offset,
+    });
   }
 
   async findOne(id: string) {
