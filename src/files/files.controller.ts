c8a920b34f8b9e500c3df4fac9b61e32776bba9f
Leandro Garzon <hekoej@gmail.com>
2024-10-09 22:11:44 -0500

    Servir archivos de manera controlada

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Param, UploadedFile, UseInterceptors, BadRequestException, Res } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { fileFilter, fileNamer } from './helpers';
+import { Response } from 'express';
 
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  findProductImge(
+    @Res() res: Response,
+    @Param('imageName') imageName: string
+  ) {
+
+    const path = this.filesService.getStaticProductImage( imageName );
+
+    res.sendFile( path );
+  }
 
   @Post('product')
   @UseInterceptors( FileInterceptor('file' ,{
@@ -25,7 +37,9 @@ export class FilesController {
       throw new BadRequestException('Make sure that files is an image0');
     }
 
-    return {fileName: file.originalname};
+    const secureUrl = `${ file.filename }`
+
+    return {secureUrl};
   }
   
 }
