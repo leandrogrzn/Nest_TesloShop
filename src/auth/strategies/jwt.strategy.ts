5d3624a317f844558c489a34131769f81c4bb608
Leandro Garzon <hekoej@gmail.com>
2024-10-20 17:31:19 -0500

    JwtStrategy - Parte 2

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,17 +1,43 @@
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-jwt";
+import { ExtractJwt, Strategy } from "passport-jwt";
 import { JwtPayLoad } from "../interfaces/jwt-payload.interface";
 import { User } from "../entities/user.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { ConfigService } from '@nestjs/config';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy( Strategy ) {
+
+  constructor(
+    @InjectRepository( User )
+    private readonly userRepository: Repository<User>,
+    
+    configService: ConfigService,
+  ) {
+
+    super({
+      secretOrKey: configService.get('JWT_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
 
   async validate ( payload: JwtPayLoad ): Promise<User> {
 
     const { email } = payload;
 
+    const user = await this.userRepository.findOneBy({ email });
+
+    if ( !user ) {
+      throw new UnauthorizedException('Token not valid');
+    }
     
+    if ( !user.isActive ) {
+      throw new UnauthorizedException('User is inactive, talk with an admin');
+    }
     
-    return;
+    return user;
   }
 
 }
