596190fc42bf5d0e187d52bf5ad09b9616e69a81
Leandro Garzon <hekoej@gmail.com>
2024-10-20 16:31:27 -0500

    Login de usuario

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,9 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt';
+import { LoginUserDto, CreateUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -32,6 +32,27 @@ export class AuthService {
     } catch (error) {
       this.handleDBErrors(error);
     }
+  }
+
+  async login( loginUserDto: LoginUserDto ) {
+
+    const { password, email } = loginUserDto;
+    
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true }
+    });
+
+    if ( !user ){
+      throw new UnauthorizedException('Credentials are not valid (email)');
+    }
+    
+    if ( !bcrypt.compareSync( password, user.password ) ) {
+      throw new UnauthorizedException('Credentials are not valid (password)');
+    }
+
+    return user;
+
   }
 
 
