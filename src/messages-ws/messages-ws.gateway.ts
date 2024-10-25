a8b13853a6b23d19b94c47e035bcd094fd05102c
Leandro Garzon <hekoej@gmail.com>
2024-10-25 15:06:55 -0500

    Server - Escuchar conexiones y desconexiones

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,17 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(
+    private readonly messagesWsService: MessagesWsService
+  ) {}
+
+  handleConnection(client: Socket) {
+    console.log('Cliente conectado:', client.id);
+  }
+  handleDisconnect(client: Socket) {
+    console.log('Cliente desconectado:', client.id);
+  }
 }
