e3acdd1b7e40de50480df7dcc5539dd383b86335
Leandro Garzon <hekoej@gmail.com>
2024-10-25 15:43:11 -0500

    Server - Mantener identificados los clientes

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -9,9 +9,15 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
   ) {}
 
   handleConnection(client: Socket) {
-    console.log('Cliente conectado:', client.id);
+    // console.log('Cliente conectado:', client.id);
+    this.messagesWsService.registerClient(client);
+
+    console.log({ clientes: this.messagesWsService.getConnectedClients() });
   }
   handleDisconnect(client: Socket) {
-    console.log('Cliente desconectado:', client.id);
+    // console.log('Cliente desconectado:', client.id);
+    this.messagesWsService.removeClient(client.id);
+    
+    console.log({ clientes: this.messagesWsService.getConnectedClients() });
   }
 }
